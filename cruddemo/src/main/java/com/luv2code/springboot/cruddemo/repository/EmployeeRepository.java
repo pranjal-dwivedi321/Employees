3a31c65d9cde6de8128d544b9388946af2a6710b
Pranjal's laptop <pranjaldwivedi001@gmail.com>
2024-12-25 04:23:21 +0530

    Commit 1

---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.cruddemo.repository;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+}
